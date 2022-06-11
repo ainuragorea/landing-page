524cceaed989598a453a91d4e4d32e878e5c6090
Ainura Gorea <ainuragorea@Ainuras-MacBook-Pro.local>
2022-06-11 18:51:27 +0300

    init project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import AppService from './modules/app.service'
+import {config} from './modules/config'
+import './modules/header.component'
+import './css/index.css'
+import './scss/index.scss'
+
+ console.log(css.toSting())
+console.log('Config key:', config.key)
+
+const service = new AppService('Hello world')
+service.log()
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HTMLPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+
+    plugins: [
+        new HTMLPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin ({
+            filename: 'style.css'
+        })
+    ],
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [ 
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+
+            { 
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    }    
+}
